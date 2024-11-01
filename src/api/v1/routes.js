40db27a0268588aa9df94d3c309804138e012e1e
Lê Gia Phú <phugiale2912@gmail.com>
2024-11-01 22:23:35 +0700

    init: api v1 routes

---
+++ src/api/v1/routes.js
@@ -0,0 +1,32 @@
+// --------------------------
+// Section: Package Requirements
+// --------------------------
+const router = require("express").Router();
+
+// --------------------------
+// Section: Custom Utils Requirements
+// --------------------------
+const {
+  limit10Req5Min,
+  limit100Req15Min,
+} = require("../../utils/requestLimit");
+
+// --------------------------
+// Section: Custom Middlewares
+// --------------------------
+
+// --------------------------
+// Section: Connect to Databases
+// --------------------------
+
+// --------------------------
+// Section: Routers
+// --------------------------
+// User Routers: /api/v1/users/...
+// const UsersRouter = require("./Users/UsersRouter");
+// router.use("/users", UsersRouter);
+
+// --------------------------
+// Section: Exports
+// --------------------------
+module.exports = router;
