570249b85a19543461895e8a65a5f3c1b06c26df
Lê Gia Phú <phugiale2912@gmail.com>
2024-11-01 22:33:03 +0700

    style: add UsersRouter dividers

---
+++ src/api/v1/Users/UsersRouter.js
@@ -0,0 +1,57 @@
+// --------------------------
+// Section: Package Requirements
+// --------------------------
+const router = require("express").Router();
+const createError = require("http-errors");
+
+// --------------------------
+// Section: Custom Utils Requirements
+// --------------------------
+
+// --------------------------
+// Section: Custom Middlewares
+// --------------------------
+
+// --------------------------
+// Section: Users Middlewares
+// --------------------------
+
+// --------------------------
+// Section: Users Routers
+// --------------------------
+router.route("/").get((req, res) => {
+  return res.status(200).json({
+    code: 1,
+    success: true,
+    message: "Default branch 🥚 of /users! ",
+  });
+});
+// router.route("/create").post(validateUser, createUser);
+// router.route("/all").get(getAllUsers);
+// router
+//   .route("/user/:userId")
+//   .get(getUserById)
+//   .patch(validateUser, patchUserById)
+//   .delete(deleteUserById);
+
+// --------------------------
+// Section: Users Error Handlers
+// --------------------------
+router
+  .use((req, res, next) => {
+    next(createError(404, "This /users directory does not exist!"));
+  })
+  .use((err, req, res, next) => {
+    let errorStatus = err.status || 404;
+    let errorMessage = err.message || "";
+    return res.status(errorStatus).json({
+      code: 0,
+      success: false,
+      message: errorMessage,
+    });
+  });
+
+// --------------------------
+// Section: Exports
+// --------------------------
+module.exports = router;
